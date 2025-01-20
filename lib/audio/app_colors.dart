d97514df1fe5cd9feec88245dd90d2dfa27efad7
Mubarek Adem <mubarekademy@gmail.com>
2025-01-20 10:54:06 +0300

    finished navbar

---
+++ lib/audio/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+final Color background = Color(0xFFfafafc);
+final Color silverBackground = Color(0xFFfafafc); // Fixed typo in variable name
+final Color menu1Color = Color(0xFFf9d263);
+final Color menu2Color =
+    Color(0xFFf2603d); // Fixed capitalization for consistency
+final Color menu3Color = Color(0xFF04abe6);
+final Color tabBarViewColor = Color(0xFFfdfdfd); // Fixed typo in variable name
+final Color starColor = Color(0xFFfdc746);
+final Color subTitleText = Color(0xFFc5c4ca);
+final Color loveColor = Color(0xFF00ace6);
+final Color audioBlueBackground = Color(0xFFeeeffa);
+final Color audioGreyBackground = Color(0xFFf2f2f2);
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_color/flutter_color.dart' as AppColors;
+import 'audio/app_colors.dart' as AppColors;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -12,9 +15,55 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color:AppColors.Background
+      color: AppColors.background,
       child: SafeArea(
-        child: Scaffold(),
+        child: Scaffold(
+            body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ImageIcon(AssetImage("lib/assets/img/menu.png"),
+                      size: 24, color: Colors.black),
+                  Row(
+                    children: [
+                      Icon(Icons.search),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(Icons.notifications),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child:
+                        Text("Popular Books ", style: TextStyle(fontSize: 30)))
+              ],
+            ),
+            Container(
+                height: 180,
+                child: PageView.builder(
+                    controller: PageController(viewportFraction: 0.8),
+                    itemCount: 5,
+                    itemBuilder: (_, i) {
+                      return Container(
+                          height: 180,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                image: AssetImage("lib/assets/img/pic-8.png"),
+                              )));
+                    }))
+          ],
+        )),
       ),
     );
   }
